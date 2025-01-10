4f10e99b8ded7fe35cd3e462e2617ad94f7bc2b4
javazquez <javazquez@uc-3-320.AluVedruna.local>
2025-01-10 20:09:07 +0100

    organizado , falta empezar a fuerte

---
+++ app/api/user/route.tsx
@@ -0,0 +1,73 @@
+const BEARER_TOKEN: string | undefined = process.env.PRINTFUL_API_KEY;
+
+interface ErrorResponse {
+  message: string;
+  error: string;
+}
+
+export async function GET(request: Request): Promise<Response> {
+  const params = new URL(request.url).searchParams;
+  const categories = params.get('category');
+  console.log(categories);
+
+  try {
+    let response: Response;
+    
+    if (!categories || categories === null) {
+      response = await fetch(`https://api.printful.com/store/products`, {
+        headers: {
+          'Authorization': `Bearer ${BEARER_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      
+    } else {
+      const categoryList = categories.split(',').map(id => id.trim());
+      console.log(`Fetching products for categories: ${categoryList}`);
+      const categoryParams = categoryList.map(id => `category_id=${id}`).join('&');
+      response = await fetch(`https://api.printful.com/store/products?${categoryParams}`, {
+        headers: {
+          'Authorization': `Bearer ${BEARER_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    console.log(response);
+    
+    if (!response.ok) {
+      const errorData: ErrorResponse = await response.json();
+      return new Response(JSON.stringify(errorData), { 
+        status: response.status, 
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
+
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error: unknown) {
+    // Verificamos si el error es una instancia de Error y tiene la propiedad 'message'
+    if (error instanceof Error) {
+      const errorResponse: ErrorResponse = {
+        message: 'Internal Server Error',
+        error: error.message || 'Unknown Error',
+      };
+      return new Response(JSON.stringify(errorResponse), { 
+        status: 500, 
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
+
+    // Si el error no es una instancia de Error, devolvemos un error genérico
+    const errorResponse: ErrorResponse = {
+      message: 'Internal Server Error',
+      error: 'Unknown Error',
+    };
+    return new Response(JSON.stringify(errorResponse), { 
+      status: 500, 
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+}
